bd2dd56f70295473d162c4c7cbc4f21d8a5ed1a2
bironsecret <44505718+bironsecret@users.noreply.github.com>
2021-10-15 16:02:14 +0300

    added functionability, ready for first release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="visualizer-torch",
-    version="0.0.3",
+    version="0.0.4",
     author="neonsecret",
     long_description=long_description,
     long_description_content_type="text/markdown",
